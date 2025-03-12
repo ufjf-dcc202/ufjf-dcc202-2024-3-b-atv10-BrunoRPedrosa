23089307a7bbf2db9a7c22e69c5ef143080aa1d3
BrunoRPedrosa <bruno.pedrosa@estudante.ufjf.br>
2025-03-12 18:23:30 -0300

    Terminando os slides

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+import {
+  getJoao,
+  getMaria,
+  setJoao,
+  setMaria,
+  deJoaoParaMaria,
+  deMariaParaJoao,
+} from "./joaoEMaria";
+
 console.log("módulomain");
+
+console.log("João tem ", getJoao(), "maçãs!");
+console.log("Maria tem ", getMaria(), "maçãs!");
